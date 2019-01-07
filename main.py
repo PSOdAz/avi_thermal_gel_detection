a0b9b515a5f8d53d003fe8fde23b637d49307407
Pornchai Soda <Pornchai_Soda@Jabil.com>
2019-01-07 16:10:58 +0800

    'update'

--- main.py
+++ main.py
@@ -1,48 +1,36 @@
 import os
-import pathlib as Path
-
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def listdir_fullpath(d):
     return [os.path.join(d, f) for f in os.listdir(d)]
 
 def template_matching(image, templates):
-    image = cv2.imread(image, -1)
-    result = image.copy()
     
-    for templ in templates :
-        
-        templ = cv2.imread(templ, -1)
-        result = cv2.matchTemplate(image, templ, cv2.TM_CCOEFF)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-        top_left = max_loc
-        h, w = templ.shape
-        bottom_right = (top_left[0] + w, top_left[1] + h)
-        cv2.rectangle(result, top_left, bottom_right,(0,0,255),4)
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('1', image)
-    cv2.imshow('2', result)
-    cv2.waitKey(0)
-    return image, result
+    for template in templates :
+        templ = cv2.imread(template, 0)
+        w, h = templ.shape[::-1]
 
+        res = cv2.matchTemplate(img_gray, templ, cv2.TM_CCOEFF_NORMED)
+        threshold = 0.8
+        loc = np.where( res >= threshold)
+        for pt in zip(*loc[::-1]):
+            cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0,255,0), 2)
 
-dir = os.getcwd()
-template_dir = os.path.join(dir, 'templates')
-template_files = listdir_fullpath(template_dir)
+    return image
 
-image1 = os.path.join(dir, 'THERMAL_GAL_NAVI_0_PASS.png')
-image2 = os.path.join(dir, 'THERMAL_GAL_NAVI_0_FAIL.png')
+_dir = os.path.join(os.getcwd(), 'templates')
 
-image1, result_image1 = template_matching(image1, template_files)
-image2, result_image2 = template_matching(image2, template_files)
+templates = listdir_fullpath(_dir)
+img1 = cv2.imread(os.path.join(os.getcwd(), 'THERMAL_GAL_NAVI_0_PASS.png'))
+img2 = cv2.imread(os.path.join(os.getcwd(), 'THERMAL_GAL_NAVI_0_FAIL.png'))
+img3 = cv2.imread(os.path.join(os.getcwd(), 'heatsink.png'))
 
-# Show result
-imgBoth1 = np.dstack((image1, result_image1))
-imgBoth.shape 
-imgBoth2 = np.dstack((image2, result_image2))
-cv2.imshow('Compare Result1', imgBoth1)
-cv2.imshow('Compare Result2', imgBoth2)
+cv2.imwrite('res_img1.png', template_matching(img1, templates))
+cv2.imwrite('res_img2.png', template_matching(img2, templates))
+cv2.imwrite('res_img3.png', template_matching(img3, templates))
 
-cv2.waitKey(0)
